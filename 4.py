68bdc1c2e61fe734a6078dd59b6cb57d12f2d693
theforestval <theforestval@gmail.com>
2024-02-21 16:39:46 +0300

    practice2

---
+++ 4.py
@@ -0,0 +1,8 @@
+colour1 = input("Введите первый цвет")
+colour2 = input("Введите второй цвет")
+if colour1 == "красный" and colour2 == "синий" or colour1 == "синий" and colour2 == "красный":
+    print("фиолетовый")
+if colour1 == "красный" and colour2 == "желтый" or colour1 == "желтый" and colour2 == "красный":
+    print('оранжевый')
+if colour1 == "синий" and colour2 == "желтый" or colour1 == "желтый" and colour2 == "синий":
+    print('зеленый')
