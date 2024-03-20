2a48e6121f0aaf4c1ad7bbf61ce00e792dad67e9
Cosider24 <torhovvlad5@gmail.com>
2024-03-20 17:04:44 +0300

    practice4

--- 4.1.py
+++ 4.1.py
@@ -1,12 +1,14 @@
-a = int(input('Введите число '))
 
 def f():
+    a = int(input('Введите число '))
     if a%7==0:
         print('Число делится на семь', a)
     else:
         print('Число не делится на семь', a)
 
+
 def s():
+    a = int(input('Введите число '))
     try:
         a = int(input('Введите число '))
         b = a//200
@@ -14,7 +16,24 @@ def s():
     except (ValueError, ZeroDivisionError):
         print('Ошибка')
 
+
 def t():
+    a = str(input('Введите дату '))
+    b = a.split('.')
+    if int(b[0])*int(b[1])==int(b[2]):
+        print('Число магическое')
+        return False
+    else:
+        print('Число немагическое')
+        return True
+
+t()
+
+
+
+
+
+
     
 
 
