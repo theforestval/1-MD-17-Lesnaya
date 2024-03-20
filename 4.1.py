b4bb64d48ee2a5ef34ffc8a7efeb950a09424fca
Cosider24 <torhovvlad5@gmail.com>
2024-03-20 16:34:56 +0300

    practice4

--- 4.1.py
+++ 4.1.py
@@ -1,5 +1,21 @@
 a = int(input('Введите число '))
-if a%7==0:
-    print('Число делится на семь')
-else:
-    print('Число не делится на семь')
+
+def f():
+    if a%7==0:
+        print('Число делится на семь', a)
+    else:
+        print('Число не делится на семь', a)
+
+def s():
+    try:
+        a = int(input('Введите число '))
+        b = a//200
+        return b
+    except (ValueError, ZeroDivisionError):
+        print('Ошибка')
+
+def t():
+    
+
+
+
