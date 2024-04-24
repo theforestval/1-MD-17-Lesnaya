d070481a89bd974d178fbf448868e0d525925af4
theforestval <ke1suke@mail.ru>
2024-04-24 16:50:00 +0300

    practice8

---
+++ 8.py
@@ -0,0 +1,37 @@
+def first():
+    from PIL import Image
+    alyona = Image.open('alyona.jpg')
+    crop = alyona.crop((300, 50, 900, 700))
+    crop.show()
+    crop.save('alyona_crop.jpg')
+
+def second():
+    from PIL import Image
+    dict = {"новый год": "новыйгод.jpg", "восьмое марта": "8марта.jpg" }
+    holiday = input("Открытка к какому празднику вам нужна? ")
+    if holiday in dict:
+        card = Image.open(dict[holiday])
+        card.show()
+    else:
+        print("Открытка к данному празднику не найдена")
+
+
+def third():
+    from PIL import Image, ImageDraw, ImageFont
+    alyona = Image.open('alyona.jpg')
+    crop = alyona.crop((300, 50, 900, 700))
+    name = input("Введите имя человека, которого хотите поздравить ")
+    font = ImageFont.truetype("arial.ttf", 30)
+    draww = ImageDraw.Draw(crop)
+    draww.text((270, 550), name, font=font, fill='purple')
+    draww.text((240, 600), "поздравляю!", font=font, fill='pink')
+    crop.show()
+
+third()
+
+
+
+
+
+
+
